0b742df59796b28b070c96cad96c67f938aa3a08
xbinh47 <91370679+xbinh47@users.noreply.github.com>
2023-04-09 13:16:25 +0700

    Fix Error Message

--- MidTerm/source_code/midterm/src/main/java/midterm/application/controller/CustomErrorController.java
+++ MidTerm/source_code/midterm/src/main/java/midterm/application/controller/CustomErrorController.java
@@ -17,10 +17,32 @@ import static org.aspectj.bridge.MessageUtil.error;
 public class CustomErrorController implements ErrorController {
     @GetMapping(value = "/error")
     public String HandleError(HttpServletRequest request, Model model){
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) == null ? 500 : request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Object errorMsg = request.getAttribute(RequestDispatcher.ERROR_MESSAGE) == null ? "Internal Server Error" : request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errorMsg = null;
+        if(status != null){
+            Integer statusCode = Integer.valueOf(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                errorMsg = "404 - Page not found";
+            }else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                errorMsg = "500 - Internal Server Error";
+            }else if(statusCode == HttpStatus.FORBIDDEN.value()){
+                errorMsg = "403 - Forbidden";
+            }else if(statusCode == HttpStatus.BAD_REQUEST.value()){
+                errorMsg = "400 - Bad Request";
+            }
+            else if(statusCode == HttpStatus.METHOD_NOT_ALLOWED.value()){
+                errorMsg = "405 - Method Not Allowed";
+            }
+            else if(statusCode == HttpStatus.UNAUTHORIZED.value()){
+                errorMsg = "401 - Unauthorized";
+            }
+            else{
+                errorMsg = "Something went wrong";
+            }
+        }
         model.addAttribute("errorMsg",errorMsg);
         model.addAttribute("errorCode",status);
         return "error";
     }
+
 }
