2f70ba531a197d4393a882798ffef1b4c31b2b6f
xbinh47 <91370679+xbinh47@users.noreply.github.com>
2023-04-12 08:36:29 +0700

    Done lab 8

--- Lab 7/Lab7_1/src/main/java/reporsitory/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package reporsitory;
-import module.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
--- Lab 7/Lab7_3/src/main/java/application/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repository;
-import application.module.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
